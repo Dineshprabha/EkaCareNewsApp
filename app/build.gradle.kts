fefd8605d7a05c58f5ce3346b9c9953d1959ae72
Dinesh N <dinesh.prabha1205@gmail.com>
2025-01-10 22:01:02 +0530

    Add API_KEY to secret.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -23,6 +25,14 @@ android {
         }
     }
 
+    val localProperties = Properties()
+    val localPropertiesFile = File(rootDir, "secret.properties")
+    if(localPropertiesFile.exists() && localPropertiesFile.isFile){
+        localPropertiesFile.inputStream().use {
+            localProperties.load(it)
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -30,6 +40,11 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "API_KEY", localProperties.getProperty("API_KEY"))
+        }
+
+        debug {
+            buildConfigField("String", "API_KEY", localProperties.getProperty("API_KEY"))
         }
     }
     compileOptions {
@@ -42,6 +57,7 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+        resValues = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
--- app/src/main/java/com/dinesh/ekacarenewsapp/data/remote/NewsApi.kt
+++ app/src/main/java/com/dinesh/ekacarenewsapp/data/remote/NewsApi.kt
@@ -1,5 +1,6 @@
 package com.dinesh.ekacarenewsapp.data.remote
 
+import com.dinesh.ekacarenewsapp.BuildConfig
 import com.dinesh.ekacarenewsapp.data.remote.dto.NewsResponse
 import com.dinesh.ekacarenewsapp.utils.Constants
 import retrofit2.Response
@@ -11,6 +12,6 @@ interface NewsApi {
     @GET("everything")
     suspend fun getNews(
         @Query("q") query: String = "technology",
-        @Query("apikey") apikey : String = Constants.API_KEY
+        @Query("apikey") apikey : String = BuildConfig.API_KEY
     ):NewsResponse
 }
